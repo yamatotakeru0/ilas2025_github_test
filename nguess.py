1f6191e7a7231281eedfcfe4196be60bedc7c9ba
yamatotakeru <yamaken060615@outlook.com>
2025-06-02 12:17:58 +0900

    give up追加0602-(0)

--- nguess.py
+++ nguess.py
@@ -1,12 +1,14 @@
 import random
 answer = random.randint(1, 100)
-#print("answer is" , answer)
 guess = int(input('guess='))
 print('Your guess is', guess)
 truth = 0
 while truth == 0 :
     if guess == answer :
         print('Good guess')
+        truth = 1
+    elif guess == 0 :
+        print('The answer is', answer)
         truth = 1
     else :
         guess = int(input('guess='))
